cba2fde22fae179e3eabf77c112be15178784e0e
Mehul <mehtomar@gmail.com>
2024-10-15 22:05:02 -0400

    Implemented initLogger and closeLogger

--- include/logging/log.h
+++ include/logging/log.h
@@ -1,7 +1,19 @@
 #ifndef _LOGGING_H_
 #define _LOGGING_H_
 
-void testingFunction(int val);
+#include <stdio.h>
+
+typedef enum{
+    TRACE,
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR
+} SeverityLevel;
+
+void initLogger(const char *, SeverityLevel);
+void logMessage(const char *, SeverityLevel);
+void closeLogger(void);
 
 #endif
 
--- src/log.c
+++ src/log.c
@@ -1,7 +1,22 @@
 #include <logging/log.h>
-#include <stdio.h>
 
-void testingFunction(int val) {
-    printf("%d\n", val);
+static FILE *logOut = stdout;
+static SeverityLevel minLevel = TRACE;
+
+void initLogger(const char *filename, SeverityLevel minimumLevel) {
+    if (filename)
+        logOut = fopen(filename, "a");
+
+    minLevel = minimumLevel;
+}
+
+void logMessage(const char *msg, SeverityLevel level) {
+}
+
+void closeLogger(void) {
+    if (logOut != stdout) {
+        fclose(logOut);
+        logOut = stdout;
+    }
 }
 
